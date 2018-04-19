d37aab80b1a9c41415c58f2c7c897c59a729ea25
micjoyce <micjoyce90@gmail.com>
2018-04-19 14:19:05 +0800

    init gcanvas for ios

--- App.js
+++ App.js
@@ -9,36 +9,113 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  Button
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  requireNativeComponent,
+  findNodeHandle,
+  NativeModules
+} from 'react-native';
 
-type Props = {};
-export default class App extends Component<Props> {
+
+import {
+  GCanvasView,
+} from 'react-native-gcanvas';
+
+import { enable, ReactNativeBridge, Image as GImage } from "gcanvas.js";
+
+ReactNativeBridge.GCanvasModule = NativeModules.GCanvasModule;
+ReactNativeBridge.Platform = Platform;
+
+ReactNativeBridge.GCanvasModule.setLogLevel(0);
+
+export default class App extends Component<{}> {
+
+  canvasCtx = null;
+
+  canvasRef = null;
+
+  onPressHandle = () => {
+    console.log(">>>>>>>>onPressHandle...start")
+
+    var ref = this.refs.canvas_holder;
+
+    var canvas_tag = findNodeHandle(ref);
+    // var canvas_tag = "2";
+    var el = { ref:""+canvas_tag, style:{width:414, height:376}};
+
+    ref = enable(el, {bridge: ReactNativeBridge, disableAutoSwap: true});
+
+    this.canvasRef = ref;
+
+    var ctx = ref.getContext('2d');
+    
+
+    this.canvasCtx = ctx
+
+    //rect
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, 100, 100);
+
+    //rect
+    ctx.fillStyle = 'black';
+    ctx.fillRect(100, 100, 100, 100);
+    ctx.fillRect(25, 205, 414-50, 5);
+
+    //circle
+    ctx.arc(200, 315, 100, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    var image = new GImage();
+    image.onload = function(){
+      ctx.drawImage(image, 150, 0);
+
+      ctx.drawImage(image, 150, 450);
+
+      ref._swapBuffers();
+    }
+    image.src = 'https://gw.alicdn.com/tfs/TB1KwRTlh6I8KJjy0FgXXXXzVXa-225-75.png';
+
+
+    ref._swapBuffers();
+
+    console.log(">>>>>>>>onPressHandle...end")
+  };
+  handleOnpress = () => {
+    //rect
+    this.canvasCtx.fillStyle = 'blue';
+    this.canvasCtx.fillRect(200, 200, 100, 100);
+    this.canvasRef._swapBuffers();
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View>
+        <Text>
+          Click to draw gcanvas
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button 
+          onPress={this.handleOnpress}
+          title={'Click to draw redraw gcanvas'}
+        />
+        <TouchableHighlight onPress={this.onPressHandle}>
+          <GCanvasView ref='canvas_holder' style={styles.gcanvas}>
+          </GCanvasView>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  gcanvas: {
+    top: 20,
+    width: 414,
+    height :700,
+    backgroundColor: '#FF000030'
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -46,13 +123,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 50,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    top:20,
+    height :40
+  }
 });
