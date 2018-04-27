487e2a097ac0278beb2f1c37ffa92ff485b7394a
micjoyce <micjoyce90@gmail.com>
2018-04-27 13:30:15 +0800

    add more examples and preview

--- App.js
+++ App.js
@@ -45,12 +45,13 @@ ReactNativeBridge.GCanvasModule.setLogLevel(0);
 
 
 const deviceScale = PixelRatio.get();
-const containerLayout = { width:width, height: (height - 200) }
+const containerLayout = { width:width, height: (height - 140) }
 const canvasLayout = { width: containerLayout.width * deviceScale, height: containerLayout.height * deviceScale }
 
 const eventCentral = new EV();
 
 let count = 0
+
 
 export default class App extends Component<{}> {
 
@@ -69,6 +70,30 @@ export default class App extends Component<{}> {
   cursorCanvas = null;
 
   cursorChart = null
+
+  menuTypes = {
+    'MIN': 'handleMin',
+    'DAY5': 'handleFiveDay',
+    'DAY': 'handleKline',
+    'WEEK': 'handleKline',
+    'MON': 'handleKline',
+    'M5': 'handleKline',
+    'SEER': 'handleSeer'
+  }
+
+  state = {
+    typeMenusCfg: [
+      { type: 'MIN', fc: 'SH000001', label_en: '1 min(Idx)', label_zh: '分时(指数)' },
+      { type: 'DAY5', fc: 'SH000001', label_en: '5 days(Idx)', label_zh: '五日(指数)' },
+      { type: 'MIN', fc: 'SZ300545', label_en: '1 min', label_zh: '分时' },
+      { type: 'DAY5', fc: 'SZ300545', label_en: '5 days', label_zh: '五日' },
+      { type: 'DAY', fc: 'SZ300545', label_en: '1 day', label_zh: '日K' },
+      { type: 'WEEK', fc: 'SZ300545', label_en: '1 week', label_zh: '周线' },
+      { type: 'MON', fc: 'SZ300545', label_en: '1 month', label_zh: '月线' },
+      { type: 'M5', fc: 'SZ300545', label_en: '5 min', label_zh: '5分钟' },
+      { type: 'SEER', fc: 'SZ300545', label_en: 'seer(extend)', label_zh: '预测' }
+    ]
+  }
 
   initChart = () => {
     if (!this.mainCanvas || !this.cursorCanvas || !this.cursorContext) {
@@ -187,6 +212,47 @@ export default class App extends Component<{}> {
     this.mainChart.onPaint()
   }
 
+  handleFiveDay = (code) => {
+    console.log('Five Day Line')
+    this.mainChart.clear()
+    this.mainChart.initData(20180413, ClChart.DEF_DATA.STOCK_TRADETIME)
+    this.mainChart.setData('INFO', ClChart.DEF_DATA.FIELD_INFO, getMockData(code, 'INFO'))
+    this.mainChart.setData('DAY5', ClChart.DEF_DATA.FIELD_DAY5, getMockData(code, 'DAY5'))
+    this.mainChart.setData('MIN', ClChart.DEF_DATA.FIELD_MIN, getMockData(code, 'MIN'))
+    const mainHeight = canvasLayout.height * 2 / 3
+    const mainLayoutCfg = {
+      layout: ClChart.DEF_CHART.CHART_LAYOUT,
+      config: ClChart.DEF_CHART.CHART_DAY5,
+      rectMain: {
+        left: 0,
+        top: 0,
+        width: canvasLayout.width,
+        height: mainHeight
+      }
+    }
+    const KBarChart = this.mainChart.createChart('DAY5', 'CHART.LINE', mainLayoutCfg, function (result) {
+      //  console.log(result)
+    })
+    this.mainChart.bindData(KBarChart, 'DAY5')
+
+    const volumeLoyoutCfg = {
+      layout: ClChart.DEF_CHART.CHART_LAYOUT,
+      config: ClChart.DEF_CHART.CHART_DAY5VOL,
+      rectMain: {
+        left: 0,
+        top: mainHeight,
+        width: canvasLayout.width,
+        height: canvasLayout.height - mainHeight
+      }
+    }
+    const KVBarChart = this.mainChart.createChart('VLINE5', 'CHART.LINE', volumeLoyoutCfg, function (result) {
+      //  console.log(result)
+    })
+    this.mainChart.bindData(KVBarChart, 'DAY5')
+
+    this.mainChart.onPaint()
+  }
+
   // 画日线
   handleKline = (code, peroid) => {
     let source = peroid
@@ -298,54 +364,39 @@ export default class App extends Component<{}> {
     this.mainChart.onPaint()
   }
 
+  drawChart (data) {
+    data = data || {}
+    const drawFunc = this.menuTypes[data.type]
+    if (typeof this[drawFunc] === 'function') {
+      this[drawFunc](data.fc, data.type)
+    }
+  }
+
+  _renderButtons() {
+    return (
+      <View style={{height: 120, backgroundColor: '#ccc', alignContent: 'center', justifyContent: 'center', flexDirection: 'row', flexWrap: 'wrap'}}>
+        {this.state.typeMenusCfg.map(item => {
+          return (
+            <Button 
+              key={item.fc + item.type}
+              onPress={() => {
+                this.drawChart(item)
+              }}
+              title={item.label_zh}
+            />
+          )
+        })}
+      </View>
+    )
+  }
+
   render() {
     return (
       <View>
         <View
           style={{flexDirection: 'row', marginTop: 20, flexWrap: 'wrap'}}
         >
-          <Button 
-            style={styles.button}
-            onPress={() => {
-              this.handleMin('SH000001')
-            }}
-            title={'SH000001 MIN'}
-          />
-          <Button 
-            style={styles.button}
-            onPress={() => {
-              this.handleMin('SZ300545')
-            }}
-            title={'SZ300545 MIN'}
-          />
-          <Button 
-            style={styles.button}
-            onPress={() => {
-              this.handleKline('SZ300545', 'DAY')
-            }}
-            title={'SZ300545 DAY'}
-          />
-          <Button 
-            style={styles.button}
-            onPress={() => {
-              this.handleKline('SZ300545', 'WEEK')
-            }}
-            title={'SZ300545 WEEK'}
-          />
-          <Button 
-            style={styles.button}
-            onPress={() => {
-              this.handleSeer()
-            }}
-            title={'SZ300545 SEER'}
-          />
-          <Button 
-            style={styles.button}
-            onPress={() => {
-              alert(count)
-            }}
-            title={'次数'}
-          />
+          {this._renderButtons()}
         </View>
         <View
           style={[containerLayout, styles.canvasContainer]}
@@ -404,9 +455,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  button: {
-    width: 80,
   },
   welcome: {
     fontSize: 50,
--- README.md
+++ README.md
@@ -4,22 +4,30 @@
 
 ## Running
 
-#### Clone & install
+### Clone & install
 
 * Clone this repo `git clone https://github.com/seerline/clchart.git`
 * `cd samples/RNClChartDemo`
 * run `yarn` or `npm install`
 
-#### iOS
+### iOS
 
 * Run `react-native run-ios`
 
-#### Android(Todo)
+### Android(Todo)
 
 > [GCanvas](https://github.com/alibaba/GCanvas) is difficulties integrating on React Native android platform, so android not support right now.
 
 * Run `android avd` and start an emulator
 * Run `react-native run-android`
+
+## Examples
+
+<img src="./source/min_idx.png" alt="min_idx" width="300" height="auto">
+<img src="./source/day.png" alt="day" width="300" height="auto">
+<img src="./source/5day.png" alt="5day" width="300" height="auto">
+<img src="./source/seer.png" alt="seer" width="300" height="auto">
+
 
 ## License
 
